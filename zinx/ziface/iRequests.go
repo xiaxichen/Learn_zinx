6a8d8e2c4f696216eb19e61290b394ae791fffbc
xiaxichen <xiaxichen1@163.com>
2020-11-28 17:23:49 +0800

    add new fucntion GetId

--- zinx/ziface/iRequests.go
+++ zinx/ziface/iRequests.go
@@ -3,7 +3,7 @@ package ziface
 /*
 	IRequests接口
 	实际上是吧client请求的链接信息和数据包装成到了一个Request中
- */
+*/
 
 type IRequest interface {
 	//得到当前链接
@@ -11,4 +11,7 @@ type IRequest interface {
 
 	//得到请求的消息数据
 	GetData() []byte
+
+	//获取数据的Id
+	GetId() uint32
 }
