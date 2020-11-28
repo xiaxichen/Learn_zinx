455f13505c4dc51c0bec2bcd06a6ee04ea58dfaf
xiaxichen <xiaxichen1@163.com>
2020-11-28 20:54:32 +0800

    add new Requesrs msg handler

---
+++ zinx/demo/PingRouterMsg.go
@@ -0,0 +1,37 @@
+package demo
+
+import (
+	Log "github.com/sirupsen/logrus"
+	"learn_zinx/zinx/ziface"
+	"learn_zinx/zinx/znet"
+)
+
+// ping test 路由
+type PingRouterMsg struct {
+	znet.BaseRouter
+}
+
+func (router *PingRouterMsg) PreHandle(request ziface.IRequest) {
+	Log.Info("Call Router PreHandle")
+	err := request.GetConnection().Send(request.GetId(), []byte("before ping..."))
+	if err != nil {
+		Log.Errorf("Call Router PreHandle err:%s", err)
+	}
+}
+
+func (router *PingRouterMsg) Handle(request ziface.IRequest) {
+	Log.Info("Call Router Handle")
+	Log.Infof("recv from client: msgID=%d\tData=%s", request.GetId(), string(request.GetData()))
+	err := request.GetConnection().Send(request.GetId(), []byte("ping ping ping"))
+	if err != nil {
+		Log.Errorf("Call Router Handler err:%s", err)
+	}
+}
+
+func (router *PingRouterMsg) PostHandle(request ziface.IRequest) {
+	Log.Info("Call Router PostHandle")
+	err := request.GetConnection().Send(request.GetId(), []byte("after ping..."))
+	if err != nil {
+		Log.Errorf("Call Router PostHandle err:%s", err)
+	}
+}
