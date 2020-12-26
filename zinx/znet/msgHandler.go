052d237dc217da2c9ed0a4cffdf5bb0415f43df8
xiaxichen <xiaxichen1@163.com>
2020-12-27 00:08:08 +0800

    relize StartWorkerPool & add MsgHandle func StartWorker

--- zinx/znet/msgHandler.go
+++ zinx/znet/msgHandler.go
@@ -14,6 +14,8 @@ type MsgHandle struct {
 	TaskQueue []chan ziface.IRequest
 	// 业务工作worker池的worker数量
 	WorkerPoolSize uint32
+	// 消息队列的最大长度
+	MaxWorkerTaskLen uint32
 }
 
 func (mh *MsgHandle) DoMsgHandler(request ziface.IRequest) {
@@ -44,6 +46,32 @@ func NewMsgHandle() *MsgHandle {
 	return &MsgHandle{
 		Apis:           make(map[uint32]ziface.IRouter),
 		WorkerPoolSize: utils.GlobalObject.WorkerPoolSize,
-		TaskQueue:      make([]chan ziface.IRequest, utils.GlobalObject.MaxPackageSize),
+		TaskQueue:      make([]chan ziface.IRequest, utils.GlobalObject.WorkerPoolSize),
+		MaxWorkerTaskLen: utils.GlobalObject.MaxWorkerTaskLength,
+	}
+}
+
+// 启动一个Worker工作池 开启工作池的动作，一个zinx框架只能有一个工作池
+func (mh *MsgHandle) StartWorkerPool()  {
+	// 根据WorkerPoolSize 分别开启Worker，每个Worker用一个go来承载
+	for i := 0; i < int(mh.WorkerPoolSize); i++ {
+		// 一个worker被启动
+		// 1.当前的 worker对应的channel消息队列 开辟空间 worker id为i
+		mh.TaskQueue[i] = make(chan ziface.IRequest,mh.MaxWorkerTaskLen)
+		// 2.启动当前的worker，阻塞等待消息从channel传递进来
+		go mh.StartWorker(i,mh.TaskQueue[i])
+	}
+}
+
+//启动一个工作流程
+func (mh *MsgHandle) StartWorker(workerId int,taskQueue chan ziface.IRequest) {
+	logger.Log.Info("Worker ID=%d is Started!",workerId)
+	// 不断的阻塞等待消息对应的队列消息
+	for  {
+		select {
+		// 如果有消息进入，出列的就是一个客户端的Request，执行当前request 绑定的业务
+		case request:=<-taskQueue:
+			mh.DoMsgHandler(request)
+		}
 	}
 }
