5d35f406a3e5edd93dff40b568e31531cae43b20
xiaxichen <xiaxichen1@163.com>
2021-01-01 16:16:41 +0800

    add GetConnMgr & add content

--- zinx/ziface/ISever.go
+++ zinx/ziface/ISever.go
@@ -1,13 +1,15 @@
 package ziface
 
-//定义接口方法
+// 定义接口方法
 type IServer interface {
-	//启动服务器
+	// 启动服务器
 	Start()
-	//停止服务器
+	// 停止服务器
 	Stop()
-	//运行服务器
+	// 运行服务器
 	Server()
-	//路由功能：给当前服务器注册一个路由方法，供client的链接处理使用
+	// 路由功能：给当前服务器注册一个路由方法，供client的链接处理使用
 	AddRouter(msgId uint32, router IRouter)
+	// 获取连接管理器
+	GetConnMgr() IConnManager
 }
