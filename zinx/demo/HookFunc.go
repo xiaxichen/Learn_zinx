5aec79a9caf78b553c1492ea857db3c0becfa789
xiaxichen <xiaxichen1@163.com>
2021-01-01 20:13:45 +0800

    change OnStartFunc & OnStopFunc add result

--- zinx/demo/HookFunc.go
+++ zinx/demo/HookFunc.go
@@ -7,8 +7,10 @@ import (
 
 func OnStartFunc(connection ziface.IConnection) {
 	logger.Log.Infof("Call OnStartFunc connection id %d", connection.GetConnID())
+	connection.Send(200,[]byte("Call OnStartFunc connection BEGIN"))
 }
 
 func OnStopFunc(connection ziface.IConnection) {
 	logger.Log.Infof("Call OnStopFunc connection id %d", connection.GetConnID())
+	connection.Send(200,[]byte("Call OnStartFunc connection After"))
 }
