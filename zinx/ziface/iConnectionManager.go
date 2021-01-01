3d189950df2769d01deebbba3be5802e95346f08
xiaxichen <xiaxichen1@163.com>
2021-01-01 16:17:05 +0800

    change format

--- zinx/ziface/iConnectionManager.go
+++ zinx/ziface/iConnectionManager.go
@@ -4,14 +4,14 @@ package ziface
 连接管理模块抽象层
 */
 type IConnManager interface {
-	//添加链接
+	// 添加链接
 	Add(connection IConnection)
-	//删除链接
+	// 删除链接
 	Remove(connection IConnection) error
-	//根据ConnId获取链接
+	// 根据ConnId获取链接
 	Get(connID uint32) (IConnection, error)
-	//得到当前链接数
+	// 得到当前链接数
 	Len() int
-	//清除并终止所有链接
+	// 清除并终止所有链接
 	Clear()
 }
